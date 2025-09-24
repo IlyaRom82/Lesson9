d778f9184c9744c3c262d173794eae1d9aef564b
IlyaRom82 <light108@tutamail.com>
2025-09-24 16:11:07 +0300

    Добавлены файлы из 09_lesson для Pull Request

---
+++ main/test_insert_student.py
@@ -0,0 +1,12 @@
+from sqlalchemy import text
+
+
+def test_insert_student(connection):
+    connection.execute(
+        text("INSERT INTO students (id, name) VALUES (1, 'Alice')")
+    )
+    result = connection.execute(
+        text("SELECT * FROM students WHERE id=1")
+    ).mappings().fetchone()  # возвращает dict-подобный объект
+
+    assert result['name'] == 'Alice'
